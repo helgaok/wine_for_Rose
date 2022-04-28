49082bcc2184d014c00340a1213306b8b0d11eca
helgaok <ok.09@live.ru>
2022-04-28 13:56:31 +0300

    Правки в main и template
    Постаралась выполнить улучшения 1-5, 7 - 15.

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import datetime, pandas, pprint
+import datetime
 from collections import defaultdict
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def main():
+  env = Environment(
+      loader=FileSystemLoader('.'),
+      autoescape=select_autoescape(['html', 'xml'])
+  )
 
-now_year = datetime.datetime.now()
+  template = env.get_template('template.html')
 
-excel_data_df = pandas.read_excel('wine.xlsx')
-excel_data_wine2 = pandas.read_excel('wine2.xlsx', na_values=['N/A', 'NA'], keep_default_na=False)
-excel_data_wine3 = pandas.read_excel('wine3.xlsx', na_values=['N/A', 'NA'], keep_default_na=False)
+  excel_wine3 = pandas.read_excel('wine3.xlsx', na_values=['N/A',       'NA'], keep_default_na=False)
 
-excel_dict = excel_data_df.to_dict(orient='records')
-excel_list_wine2 = list(excel_data_wine2.to_dict('records'))
-excel_list_wine3 = list(excel_data_wine3.to_dict('records'))
+  wines_from_excel = list(excel_wine3.to_dict('records'))
 
-wines_dict = defaultdict(list)
+  grouped_wines = defaultdict(list)
 
-for wine in excel_list_wine3:
-  wines_dict[wine['Категория']].append(wine)
+  for product in wines_from_excel:
+    grouped_wines[product['Категория']].append(product)
 
-print(pprint.pprint(wines_dict))
+  winery_foundation_date = 1920
 
-rendered_page = template.render(
-    age_of_winery=now_year.year - 1920,
-    wines_dict = wines_dict
-)
+  rendered_page = template.render(
+      age_of_winery=datetime.datetime.now().year -       winery_foundation_date,
+      grouped_wines = grouped_wines
+  )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+  with open('index.html', 'w', encoding="utf8") as file:
+      file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+  server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+  server.serve_forever()
+  
+if __name__ == '__main__':
+  main()
